e852f3b35be98dc5c8c6e9688770823a5f27a770
thelukelp15 <thelukelp15@gmail.com>
2022-02-28 13:25:48 -0300

    Commit

--- Parte 2/ex027.py
+++ Parte 2/ex027.py
@@ -3,7 +3,8 @@
 # o arquivo de som deve ser colado na mesma pasta onde estará o código solicitando que ele toque.
 # para copiar, basta copiar o arquivo e colar no próprio pycharm.
 import pygame
+pygame.mixer.init()
 pygame.init()
 pygame.mixer.music.load("som.mp3")
-pygame.mixer.music.play()
+pygame.mixer.music.play(loops=0,start=0)
 pygame.event.wait()
