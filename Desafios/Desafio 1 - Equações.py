208f75a8e48f6d6137f760a9c450a1f89710720b
thelukelp15 <thelukelp15@gmail.com>
2022-03-14 19:24:12 -0300

    Desafio 1 - Equação pt2

--- Desafios/Desafio 1 - Equações.py
+++ Desafios/Desafio 1 - Equações.py
@@ -1,5 +1,5 @@
-# -> Usuário pode digitar qualquer expressão matemática
-# -> expressões do tipo Z = X ? Y, onde ? = [+,-,*,/]
+# User pode digitar qualquer expressão matemática
+# expressões do tipo Z = X ? Y, onde ? = [+,-,*,/]
 print("-="*15)
 print("    RESOLUÇÃO DE EQUAÇÕES:")
 print("-="*15)
@@ -10,9 +10,8 @@ print(" - Exemplo: Z = 1 + 2")
 exp = str(input("Equação: ")).strip().lower()
 eq = exp.replace(" ", "")
 print(f"Sua equação é: {eq}")
-# transformando o texto em uma lista:
+# transformando o texto em lista:
 eqList = list(eq)
-print(eqList) # excluir
 # identificando os números:
 eqListF = list()
 for c in eqList:
@@ -20,35 +19,28 @@ for c in eqList:
         eqListF.append(int(c))
     else:
         eqListF.append(c)
-print(eqListF) # excluir
 # descobrindo onde estão os números:
 indices = list()
 for c, i in enumerate(eqListF):
     if type(i) == int:
-        print(f"Na posição {c} econtrei o valor {i}!") # excluir
         indices.append(c)
-print(indices) # excluir
 i = min(indices)
-
 d = 0
 for c in range(0, len(indices)-1):
     d = c+1
     if indices[c] + 1 != indices[d]:
         f = indices[c]
-print(f"Início - {i} ; Fim - {f}") # excluir
 # voltando a lista para str para tentar juntar os números:
-# >> obs: procurar forma de juntar int <<
-eqListT = list() # lista de transição
+# lista de transição
+eqListT = list()
 for c in eqListF:
     if type(c) == int:
         eqListT.append(str(c))
     else:
         eqListT.append(c)
-print(eqListT) # excluir
 # juntando o número:
 itens_unidos = "".join(eqListT[i:f+1])
 eqListT[i:f+1] = [itens_unidos]
-print(eqListT) # excluir
 # -------------------------------------------------
 # repetindo o processo para encontrar o outro número:
 eqListF2 = list()
@@ -57,37 +49,29 @@ for c in eqListT:
         eqListF2.append(int(c))
     else:
         eqListF2.append(c)
-print(eqListF2) # excluir
 # descobrindo onde estão os números:
 indices1 = list()
 for c, i in enumerate(eqListF2):
     if type(i) == int:
-        print(f"Na posição {c} econtrei o valor {i}!") # excluir
         indices1.append(c)
-print(indices1) # excluir
-# até aqui está ok!
-# encontrar forma de ignorar o primeiro número e juntar o segundo número.
+# ------------------------------------- ok
 d1 = 0
 for c in range(0, len(indices1)-1):
     d1 = c+1
     if indices1[c] + 1 != indices1[d1]:
         i1 = indices1[d1]
 f1 = max(indices1)
-print(f"Início - {i1} ; Fim - {f1}") # excluir
 # ------------------------------------- ok
 # voltando a lista para str para juntar os números:
-
-eqListT2 = list() # lista de transição
+# lista de transição
+eqListT2 = list()
 for c in eqListF2:
     if type(c) == int:
         eqListT2.append(str(c))
     else:
         eqListT2.append(c)
-print(eqListT2) # excluir
-
 itens_unidos2 = "".join(eqListT2[i1:f1+1])
 eqListT2[i1:f1+1] = [itens_unidos2]
-print(eqListT2) # excluir
 # transformando novamente em números:
 eqListFinal = list()
 for c in eqListT2:
@@ -95,11 +79,64 @@ for c in eqListT2:
         eqListFinal.append(int(c))
     else:
         eqListFinal.append(c)
-print(eqListFinal) # excluir
 # ------------------------------------------- ok
 # checar cada valor da equação para identificar se é um número ou letra
-# for c in eqListFinal:
-# lista = 0,1,2,3,4
-# procurar forma de validar como número ou letra um termo isolado de uma lista
+if eqListFinal.index("=") == 1:
+    if type(eqListFinal[0]) == str:
+        if "+" in eqListFinal:
+            print(f"O valor de {eqListFinal[0]} é {eqListFinal[2] + eqListFinal[4]}.")
+        elif "-" in eqListFinal:
+            print(f"O valor de {eqListFinal[0]} é {eqListFinal[2] - eqListFinal[4]}.")
+        elif "*" in eqListFinal:
+            print(f"O valor de {eqListFinal[0]} é {eqListFinal[2] * eqListFinal[4]}.")
+        elif "/" in eqListFinal:
+            print(f"O valor de {eqListFinal[0]} é {eqListFinal[2] / eqListFinal[4]}.")
+    elif type(eqListFinal[2]) == str:
+        if "+" in eqListFinal:
+            print(f"O valor de {eqListFinal[2]} é {eqListFinal[0] - eqListFinal[4]}.")
+        elif "-" in eqListFinal:
+            print(f"O valor de {eqListFinal[2]} é {eqListFinal[0] + eqListFinal[4]}.")
+        elif "*" in eqListFinal:
+            print(f"O valor de {eqListFinal[2]} é {eqListFinal[0] / eqListFinal[4]}.")
+        elif "/" in eqListFinal:
+            print(f"O valor de {eqListFinal[2]} é {eqListFinal[0] * eqListFinal[4]}.")
+    elif type(eqListFinal[4]) == str:
+        if "+" in eqListFinal:
+            print(f"O valor de {eqListFinal[4]} é {eqListFinal[0] - eqListFinal[2]}.")
+        elif "-" in eqListFinal:
+            print(f"O valor de {eqListFinal[4]} é {eqListFinal[2] - eqListFinal[0]}.")
+        elif "*" in eqListFinal:
+            print(f"O valor de {eqListFinal[4]} é {eqListFinal[0] / eqListFinal[2]}.")
+        elif "/" in eqListFinal:
+            print(f"O valor de {eqListFinal[4]} é {eqListFinal[2] / eqListFinal[0]}.")
+if eqListFinal.index("=") == 3:
+    if type(eqListFinal[0]) == str:
+        if "+" in eqListFinal:
+            print(f"O valor de {eqListFinal[0]} é {eqListFinal[4] - eqListFinal[2]}.")
+        elif "-" in eqListFinal:
+            print(f"O valor de {eqListFinal[0]} é {eqListFinal[4] + eqListFinal[2]}.")
+        elif "*" in eqListFinal:
+            print(f"O valor de {eqListFinal[0]} é {eqListFinal[4] / eqListFinal[2]}.")
+        elif "/" in eqListFinal:
+            print(f"O valor de {eqListFinal[0]} é {eqListFinal[4] * eqListFinal[2]}.")
+    elif type(eqListFinal[2]) == str:
+        if "+" in eqListFinal:
+            print(f"O valor de {eqListFinal[2]} é {eqListFinal[4] - eqListFinal[0]}.")
+        elif "-" in eqListFinal:
+            print(f"O valor de {eqListFinal[2]} é {eqListFinal[0] - eqListFinal[4]}.")
+        elif "*" in eqListFinal:
+            print(f"O valor de {eqListFinal[2]} é {eqListFinal[4] / eqListFinal[0]}.")
+        elif "/" in eqListFinal:
+            print(f"O valor de {eqListFinal[2]} é {eqListFinal[0] / eqListFinal[4]}.")
+    elif type(eqListFinal[4]) == str:
+        if "+" in eqListFinal:
+            print(f"O valor de {eqListFinal[4]} é {eqListFinal[0] + eqListFinal[2]}.")
+        elif "-" in eqListFinal:
+            print(f"O valor de {eqListFinal[4]} é {eqListFinal[0] - eqListFinal[2]}.")
+        elif "*" in eqListFinal:
+            print(f"O valor de {eqListFinal[4]} é {eqListFinal[0] * eqListFinal[2]}.")
+        elif "/" in eqListFinal:
+            print(f"O valor de {eqListFinal[4]} é {eqListFinal[0] / eqListFinal[2]}.")
+
 
 
